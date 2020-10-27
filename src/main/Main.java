43ca033568c97c035e3a42a7a997374de4426efe
Fabrício José Barrozo <ueredeveloper@gmail.com>
2020-10-27 12:34:23 -0300

    encapsulation of the marcha attribute and improvment of th acelerar method

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,19 @@ public class Main {
 	
 		Carro c1 = new Carro();
 		c1.ligar();
+		
+		// o carro acelera se estiver com freio = false
+		c1.freio = false;
+		c1.trocarMarcha();
+		c1.acelerar(19);
+		
+		c1.trocarMarcha();
+		c1.acelerar(39);
+		
+		c1.trocarMarcha();
+		c1.acelerar(89);
+		
+		c1.desligar();
 
 	}
 
--- src/model/Carro.java
+++ src/model/Carro.java
@@ -1,4 +1,7 @@
 package model;
+
+import java.util.concurrent.SynchronousQueue;
+
 /*
  * File name: Carro
  * Description: 
@@ -13,17 +16,77 @@ public class Carro {
 	// atributos
 	public String marca;
 	public int ano;
-	public int marcha;
+	/* a marcha somente pode ser mudada por um metodo para
+		que nao mude, por exemplo da marcha 1 para a 4 direto, sem passar pela 2 e 3 */
+	private int marcha;
+	public int velocidade;
+	
+	// por padrao o carro comeca freiado
+	public boolean freio = true;
+	// por padrao o carro comeca desligado
 	public boolean ligado;
+
 	
 	// metodos
 	public void ligar () {
-		System.out.println("carro ligado!");
+		if (this.ligado == false) {
+			this.ligado = true;
+			System.out.println("O carro ligado!");
+		} else {
+			System.out.println("O carro já está ligado!");
+		}
+		
 	}
 	public void desligar () {
+		if(this.ligado == true ) {
+			this.ligado = false;
+			System.out.println("O carro foi desligado!");
+		} else {
+			System.out.println("O carro não  está ligado! ");
+		}
+		this.ligado = false;
+		
+			
+	}
+	public void trocarMarcha () {
+		this.marcha = this.marcha + 1;
+		System.out.println("trocou marcha para " + this.marcha + "ª");
+	}
+	public void acelerar (int velocidade) {
+		
+		if (this.ligado == true && this.freio == false ) {
+			
+			if (this.marcha == 1) {
+				if (velocidade < 20) {
+					this.velocidade = velocidade;
+					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+				} else {
+					System.out.println("marcha desproporcional para a aceleracao");
+				}
+			} else if (this.marcha == 2) {
+				if (velocidade < 40) {
+					this.velocidade = velocidade;
+					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+				} else {
+				System.out.println("marcha desproporcional para a aceleracao");
+			}
+			} else if (this.marcha == 3) {
+				if (velocidade < 60) {
+					this.velocidade = velocidade;
+					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+				} else {
+					System.out.println("marcha desproporcional para a aceleracao");
+				}
+			} else {
+				if (velocidade < 80) {
+					this.velocidade = velocidade;
+					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+				} else {
+					System.out.println("marcha desproporcional para a aceleracao");
+				}
+			} 
 			
 		}
-	public void trocarMarcha () {
 		
 	}
 
