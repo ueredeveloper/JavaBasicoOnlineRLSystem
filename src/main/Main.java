fc4b9079d5c7a048192d0dd6ea2022ed7d611f71
ueredeveloper <ueredeveloper@gmail.com>
2020-10-27 21:07:02 +0000

    dcoder - added UTF-8 to compile javac in decoder_run

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,25 +3,25 @@ import model.*;
 
 public class Main {
 
-	//metodo principal
-	public static void main(String[] args) {
-	
-		Carro c1 = new Carro();
-		c1.ligar();
-		
-		// o carro acelera se estiver com freio = false
-		c1.freio = false;
-		c1.trocarMarcha();
-		c1.acelerar(19);
-		
-		c1.trocarMarcha();
-		c1.acelerar(39);
-		
-		c1.trocarMarcha();
-		c1.acelerar(89);
-		
-		c1.desligar();
+  //metodo principal
+  public static void main(String[] args) {
+  
+    Carro c1 = new Carro();
+    c1.ligar();
+    
+    // o carro acelera se estiver com freio = false
+    c1.freio = false;
+    c1.trocarMarcha();
+    c1.acelerar(19);
+    
+    c1.trocarMarcha();
+    c1.acelerar(39);
+    
+    c1.trocarMarcha();
+    c1.acelerar(89);
+    
+    c1.desligar();
 
-	}
+  }
 
 }
--- src/model/Carro.java
+++ src/model/Carro.java
@@ -5,7 +5,7 @@ import java.util.concurrent.SynchronousQueue;
 /*
  * File name: Carro
  * Description: 
- * 	Criar uma classe que simule os componentes de um carro
+ *   Criar uma classe que simule os componentes de um carro
  * Created: 26/10/2020 15:36
  * @Author: Fabricio Jose Barrozo
  * @Version 1.0 
@@ -13,82 +13,82 @@ import java.util.concurrent.SynchronousQueue;
  * */
 
 public class Carro {
-	// atributos
-	public String marca;
-	public int ano;
-	/* a marcha somente pode ser mudada por um metodo para
-		que nao mude, por exemplo da marcha 1 para a 4 direto, sem passar pela 2 e 3 */
-	private int marcha;
-	public int velocidade;
-	
-	// por padrao o carro comeca freiado
-	public boolean freio = true;
-	// por padrao o carro comeca desligado
-	public boolean ligado;
+  // atributos
+  public String marca;
+  public int ano;
+  /* a marcha somente pode ser mudada por um metodo para
+    que nao mude, por exemplo da marcha 1 para a 4 direto, sem passar pela 2 e 3 */
+  private int marcha;
+  public int velocidade;
+  
+  // por padrao o carro comeca freiado
+  public boolean freio = true;
+  // por padrao o carro comeca desligado
+  public boolean ligado;
 
-	
-	// metodos
-	public void ligar () {
-		if (this.ligado == false) {
-			this.ligado = true;
-			System.out.println("O carro ligado!");
-		} else {
-			System.out.println("O carro já está ligado!");
-		}
-		
-	}
-	public void desligar () {
-		if(this.ligado == true ) {
-			this.ligado = false;
-			System.out.println("O carro foi desligado!");
-		} else {
-			System.out.println("O carro não  está ligado! ");
-		}
-		this.ligado = false;
-		
-			
-	}
-	public void trocarMarcha () {
-		this.marcha = this.marcha + 1;
-		System.out.println("trocou marcha para " + this.marcha + "ª");
-	}
-	public void acelerar (int velocidade) {
-		
-		if (this.ligado == true && this.freio == false ) {
-			
-			if (this.marcha == 1) {
-				if (velocidade < 20) {
-					this.velocidade = velocidade;
-					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
-				} else {
-					System.out.println("marcha desproporcional para a aceleracao");
-				}
-			} else if (this.marcha == 2) {
-				if (velocidade < 40) {
-					this.velocidade = velocidade;
-					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
-				} else {
-				System.out.println("marcha desproporcional para a aceleracao");
-			}
-			} else if (this.marcha == 3) {
-				if (velocidade < 60) {
-					this.velocidade = velocidade;
-					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
-				} else {
-					System.out.println("marcha desproporcional para a aceleracao");
-				}
-			} else {
-				if (velocidade < 80) {
-					this.velocidade = velocidade;
-					System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
-				} else {
-					System.out.println("marcha desproporcional para a aceleracao");
-				}
-			} 
-			
-		}
-		
-	}
+  
+  // metodos
+  public void ligar () {
+    if (this.ligado == false) {
+      this.ligado = true;
+      System.out.println("O carro ligado!");
+    } else {
+      System.out.println("O carro jÃ¡ estÃ¡ ligado!");
+    }
+    
+  }
+  public void desligar () {
+    if(this.ligado == true ) {
+      this.ligado = false;
+      System.out.println("O carro foi desligado!");
+    } else {
+      System.out.println("O carro nÃ£o  estÃ¡ ligado! ");
+    }
+    this.ligado = false;
+    
+      
+  }
+  public void trocarMarcha () {
+    this.marcha = this.marcha + 1;
+    System.out.println("trocou marcha para " + this.marcha + "Â°");
+  }
+  public void acelerar (int velocidade) {
+    
+    if (this.ligado == true && this.freio == false ) {
+      
+      if (this.marcha == 1) {
+        if (velocidade < 20) {
+          this.velocidade = velocidade;
+          System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+        } else {
+          System.out.println("marcha desproporcional para a aceleracao");
+        }
+      } else if (this.marcha == 2) {
+        if (velocidade < 40) {
+          this.velocidade = velocidade;
+          System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+        } else {
+        System.out.println("marcha desproporcional para a aceleracao");
+      }
+      } else if (this.marcha == 3) {
+        if (velocidade < 60) {
+          this.velocidade = velocidade;
+          System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+        } else {
+          System.out.println("marcha desproporcional para a aceleracao");
+        }
+      } else {
+        if (velocidade < 80) {
+          this.velocidade = velocidade;
+          System.out.println("carro com velocidade "  + this.velocidade + " Km/hora.");
+        } else {
+          System.out.println("marcha desproporcional para a aceleracao");
+        }
+      } 
+      
+    }
+    
+  }
 
-	 
+   
 }
